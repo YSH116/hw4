8f815b04bcae9e02cc91a2c29f57b5ec62ed69e8
YSH116 <74852896+YSH116@users.noreply.github.com>
2021-06-14 19:27:33 +0800

    Update openmv.py

--- hw4_3/openmv.py
+++ hw4_3/openmv.py
@@ -1,1 +1,49 @@
+import pyb, sensor, image, time, math
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QQVGA) # we run out of memory if the resolution is much bigger...
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
+sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
+clock = time.clock()
+
+f_x = (2.8 / 3.984) * 160 # find_apriltags defaults to this if not set
+f_y = (2.8 / 2.952) * 120 # find_apriltags defaults to this if not set
+c_x = 160 * 0.5 # find_apriltags defaults to this if not set (the image.w * 0.5)
+c_y = 120 * 0.5 # find_apriltags defaults to this if not set (the image.h * 0.5)
+
+def degrees(radians):
+   return (180 * radians) / math.pi
+
+def cm(none):
+   return 3 * none
+
+uart = pyb.UART(3,9600,timeout_char=1000)
+uart.init(9600,bits=8,parity = None, stop=1, timeout_char=1000)
+
+while(True):
+   clock.tick()
+   img = sensor.snapshot()
+   for tag in img.find_apriltags(fx=f_x, fy=f_y, cx=c_x, cy=c_y): # defaults to TAG36H11
+      img.draw_rectangle(tag.rect(), color = (255, 0, 0))
+      img.draw_cross(tag.cx(), tag.cy(), color = (0, 255, 0))
+
+      print_args = (cm(tag.x_translation()), cm(tag.y_translation()), cm(tag.z_translation()), \
+            degrees(tag.x_rotation()), degrees(tag.y_rotation()), degrees(tag.z_rotation()))
+      print(print_args)
+      if cm(tag.x_translation()) > 0 and abs(cm(tag.z_translation())) > 2:
+        uart.write(("/turn/run 100 0.3 \n").encode())
+        print("l")
+      elif cm(tag.x_translation()) < 0 and abs(cm(tag.z_translation())) > 2:
+        uart.write(("/turn/run 100 -0.3 \n").encode())
+        print("r")
+      elif abs(cm(tag.z_translation())) < 5:
+        uart.write(("/stop/run \n").encode())
+        print("stop\n")
+      else:
+        uart.write(("/goStraight/run 100 \n").encode())
+        print("g")
+
+   print("FPS %f" % clock.fps())
 
